a7335a45ebc427e09155d26cb421b58506e334a8
Ubuntu@3060 <kimangkheang@gmail.com>
2024-04-02 19:12:09 +0700

    Remove average

--- main.py
+++ main.py
@@ -11,25 +11,6 @@ import numpy as np
 from config import Config
 
 
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-
 def main():
     # Set random seed for reproducibility
     torch.manual_seed(Config.seed)
